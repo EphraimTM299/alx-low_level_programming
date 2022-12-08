3c6134e0673374badb48c62c464034531ecdab23
EphraimTM299 <ephraimtamuka@gmail.com>
2022-12-08 05:36:38 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	double d;
+	char c;
+
+	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of an double is: %lu.\n", (unsigned long)sizeof(d));
+	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
+
+	return (0);
+}
