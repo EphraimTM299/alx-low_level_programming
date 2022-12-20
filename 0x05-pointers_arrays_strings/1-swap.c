fcb780e5ea3be05d829b1bc35cf3e7f7a027f476
EphraimTM299 <ephraimtamuka@gmail.com>
2022-12-20 05:32:09 -0800

    swap horses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * modif_my_param - set the integer to 402
+ * @m: a pointer the integer we want to set to 402
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
