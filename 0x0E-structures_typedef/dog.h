3deddf671672bbd48db2f41a341e1daa14b76b49
EphraimTM299 <ephraimtamuka@gmail.com>
2023-01-16 01:40:23 -0800

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/** struct dog - a struct that takes in name, age and owner of the dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ **/
+
+struct dog {
+	char* name;
+	float age;
+	char* owner;
+};
